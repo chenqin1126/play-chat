2a2c4b39c38357510fbe656e8ae9f43d0f70d6f3
kk <chenqin1126@126.com>
2017-01-10 12:51:24 +0800

    新增首页

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,11 +9,35 @@ import play.api.mvc._
 
 class Application @Inject()(db: Database) extends Controller {
 
-  def index = Action {
+  def index = Action({
     Ok(views.html.index("title") {
       "message2";
       "message333"
     })
+  })
+
+  def tokenEcho = Action { implicit request =>
+
+    var echostr = request.getQueryString("echostr");
+
+    var reqBody = request.body;
+
+    println("the request body is " + reqBody.asText.get)
+
+    println(echostr)
+    Ok(echostr.get)
+
+  }
+
+  def redirect = Action { implicit request => {
+
+    val adUrl = request.getQueryString("ad")
+
+    println(adUrl.get)
+
+    Results.Redirect(adUrl.get, 303)
+  }
+
   }
 
 }
